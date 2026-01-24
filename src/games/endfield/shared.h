6dec7b7bb81810498dc3af5c22c9e63d71c8ca80
SGTForgery <golubeckasarnas@gmail.com>
2026-01-24 07:53:56 +0000

    feat(endfield) Improved shadows & HDR Sun premutations

--- src/games/endfield/shared.h
+++ src/games/endfield/shared.h
@@ -84,6 +84,8 @@ struct ShaderInjectData {
   float sun_intensity;
   float bloom_strength;
   float godrays_intensity;
+  float perchannelblowout;
+  float ao_intensity;
 };
 
 #ifndef __cplusplus
@@ -137,6 +139,8 @@ cbuffer shader_injection : register(b13) {
 #define SUN_INTENSITY                          shader_injection.sun_intensity
 #define BLOOM_STRENGTH                         shader_injection.bloom_strength
 #define GODRAYS_INTENSITY                      shader_injection.godrays_intensity
+#define PER_CHANNEL_BLOWOUT                    shader_injection.perchannelblowout
+#define AO_INTENSITY                           shader_injection.ao_intensity
 
 #include "../../shaders/renodx.hlsl"
 
